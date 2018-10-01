8061d7fe605da8d43e5d9147fb055eb5d51b7a1f
reng99 <1837895991@qq.com>
2018-10-01 13:59:52 +0800

    添加html2canvas的说明

--- src/html2canvas/index.js
+++ src/html2canvas/index.js
@@ -34,7 +34,6 @@
         $html2canvas = document.getElementById('html2canvas');
 
     $btnShot.click(function(){
-        console.log('click function');
 
         // 如果你想所有版本高的浏览器支持promise对象，请使用bluebird 。官网: http://bluebirdjs.com/docs/getting-started.html
 
@@ -47,7 +46,7 @@
             useCORS: true, // 允许图片跨域,设置这个的同时这里已经在阿里云做了处理
         }).then(function(canvas){
             var dataUrl = canvas.toDataURL();
-            console.log(dataUrl);
+            // console.log(dataUrl);
             // 移除 canvas
             $html2canvas.removeChild(canvas);
             // 追加图片
@@ -63,24 +62,23 @@
      * @param {String} canvasDom canvas的id值
      */
     function setCanvas(selector , canvasDom){
-        let $sharePic = document.getElementById(selector),
-            // DOM 节点计算后的宽高
-            width = Math.ceil($sharePic.offsetWidth),
-            height = Math.ceil($sharePic.offsetHeight),
-            // 获取自定义 canvas 元素
-            canvas = document.getElementById(canvasDom),
-            ctx = canvas.getContext('2d'),
-            // 获取像素比
-            ratio = getPixelRatio(ctx);
-            
-            // 设定canvas元素属性的宽高为 DOM 节点的宽高 * 像素比
-            canvas.width = width * ratio;
-            canvas.height = height * ratio;
-            // 设定canvas css宽高为 DOM 节点宽高
-            canvas.style.height = height + 'px';
-            canvas.style.width = width + 'px';
-            // 将所有的绘制内容放大像素比倍数
-            ctx.scale(ratio,ratio);
+        let $sharePic = document.getElementById(selector),  // 获取想要转换的 DOM 节点        
+        // DOM 节点计算后宽高
+        width = Math.ceil($sharePic.offsetWidth),
+        height = Math.ceil($sharePic.offsetHeight),
+        // 获取自定义 canvas 元素
+        canvas = document.getElementById(canvasDom),
+        ctx = canvas.getContext('2d'),
+        // 获取像素比
+        ratio = getPixelRatio(ctx);
+        // 设定 canvas 元素属性宽高为 DOM 节点宽高 * 像素比
+        canvas.height = height * ratio;
+        canvas.width = width * ratio;
+        // 设定 canvas css宽高为 DOM 节点宽高
+        canvas.style.height = height + 'px';
+        canvas.style.width = width + 'px';
+        // 将所有绘制内容放大像素比倍
+        ctx.scale(ratio, ratio);
     }
 
     /**
