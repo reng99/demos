6ba927feff616bd7f095c4dcee3d620c5269a23e
reng99 <1837895991@qq.com>
2018-10-01 13:07:44 +0800

    处理图片模糊度

--- src/html2canvas/index.js
+++ src/html2canvas/index.js
@@ -30,20 +30,71 @@
     // 解决方案参考： https://stackoverflow.com/questions/48290987/uncaught-in-promise-provided-element-is-not-within-a-document
     var $shotarea = $('#shotarea')[0], 
         $resultPage = $('#shotresult'),
-        $btnShot = $('.btn-shot');
+        $btnShot = $('.btn-shot'),
+        $html2canvas = document.getElementById('html2canvas');
 
     $btnShot.click(function(){
         console.log('click function');
 
         // 如果你想所有版本高的浏览器支持promise对象，请使用bluebird 。官网: http://bluebirdjs.com/docs/getting-started.html
 
+        // 处理canvas来处理截图的清晰度问题
+        setCanvas('shotarea','canvas');
+
         html2canvas($shotarea , {
+            canvas: canvas, 
+            // scale: 2, // 这里canvas设置的时候控制
             useCORS: true, // 允许图片跨域,设置这个的同时这里已经在阿里云做了处理
         }).then(function(canvas){
             var dataUrl = canvas.toDataURL();
             console.log(dataUrl);
+            // 移除 canvas
+            $html2canvas.removeChild(canvas);
+            // 追加图片
             $resultPage.html(`<img src=${dataUrl} alt="from canvas" style="width: 100%;height: auto;"/>`);
         });
 
     });
+
+    /**
+     * 设置canvas
+     * 
+     * @param {String} selector 截取内容div的id值
+     * @param {String} canvasDom canvas的id值
+     */
+    function setCanvas(selector , canvasDom){
+        let $sharePic = document.getElementById(selector),
+            // DOM 节点计算后的宽高
+            width = Math.ceil($sharePic.offsetWidth),
+            height = Math.ceil($sharePic.offsetHeight),
+            // 获取自定义 canvas 元素
+            canvas = document.getElementById(canvasDom),
+            ctx = canvas.getContext('2d'),
+            // 获取像素比
+            ratio = getPixelRatio(ctx);
+            
+            // 设定canvas元素属性的宽高为 DOM 节点的宽高 * 像素比
+            canvas.width = width * ratio;
+            canvas.height = height * ratio;
+            // 设定canvas css宽高为 DOM 节点宽高
+            canvas.style.height = height + 'px';
+            canvas.style.width = width + 'px';
+            // 将所有的绘制内容放大像素比倍数
+            ctx.scale(ratio,ratio);
+    }
+
+    /**
+     * 获取屏幕的像素比
+     * 
+     * @param {Object} context 
+     */
+    function getPixelRatio(context) {
+        let backingStore = context.backingStorePixelRatio ||
+        context.webkitBackingStorePixelRatio ||
+        context.mozBackingStorePixelRatio ||
+        context.msBackingStorePixelRatio ||
+        context.oBackingStorePixelRatio ||
+        context.backingStorePixelRatio || 1;
+        return (window.devicePixelRatio || 1) / backingStore;
+    }
 })();
