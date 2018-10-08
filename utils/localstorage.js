ba2204faad69039cf39c59a79249ef02c29be737
reng99 <1837895991@qq.com>
2018-10-08 23:10:47 +0800

    add _getStore

--- utils/localstorage.js
+++ utils/localstorage.js
@@ -2,8 +2,18 @@
 
 // 存储localstorage
 
-
-// 获取localstorage
-
+/**
+ * 获取localstorage
+ * @param {String} name 存储的键值对的键
+ */
+function _getStore(name){
+    let obj = null;
+    try {
+        obj = window.localStorage.getItem(name) ? JSON.parse(window.localStorage.getItem(name)) : null;
+    } catch (e){
+        window.localStorage.removeItem(name);
+    }
+    return obj;
+}
 
 // 移除localstorage
