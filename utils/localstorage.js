09a3114f092b73bca254885951c277390b489b4b
reng99 <1837895991@qq.com>
2018-10-10 22:18:16 +0800

    set localStore

--- utils/localstorage.js
+++ utils/localstorage.js
@@ -1,6 +1,24 @@
 // 先占坑
 
 // 存储localstorage
+export const setStore = ({name,key,content,options}) => {
+    if(!name) return;
+    let obj = {
+        value: {},
+        expiry: options ? expiry(options.expiry) : null,
+        clean: options ? options.clean : null,
+    };
+    if(key) {
+        let tmp_obj = _getStore(name) || obj;
+        if(tmp_obj && tmp_obj.value) {
+            tmp_obj.value[key] = content;
+            obj.value = tmp_obj.value;
+        }
+    } else {
+        obj.value = content;
+    }
+    window.localStorage.setItem(name,JSON.stringify(obj));
+}
 
 /**
  * 获取localstorage
