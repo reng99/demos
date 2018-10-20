230d202787b755bf15bf688e6e655c8cec078e4a
reng99 <1837895991@qq.com>
2018-10-21 00:24:00 +0800

    complete loading version2

--- src/loadingv2/index.js
+++ src/loadingv2/index.js
@@ -5,7 +5,8 @@ $(document).ready(function(){
 });
 
 function start(){
-    console.log('start function is active');
+    $('#loading-version2-percent').hide();
+    $('#loading-version2').show();
 }
 
 function loadImage(sources , callback){
@@ -17,6 +18,7 @@ function loadImage(sources , callback){
             count++;
             var loadingRate = parseInt((count / imgNum) * 100) + '%';
             console.log(loadingRate);
+            $('#progress').text('已经加载了 '+loadingRate);
             if(count >= imgNum){
                 callback(); // 回调
             }
@@ -25,55 +27,26 @@ function loadImage(sources , callback){
     });
 }
 
-// var imagesSource = {
-//     image1 : "",
-//     image2 : "",
-//     image3 : "",
-//     image4 : "",
-//     image5 : "",
-//     image6 : "",
-//     image7 : "",
-//     image8 : "",
-//     image9 : "",
-//     image10 : "",
-//     image11 : "",
-//     image12 : "",
-//     image13 : "",
-//     image14 : "",
-//     image15 : "",
-//     image16 : "",
-//     image17 : "",
-//     image18 : "",
-// }
-
-
 var imagesSource = {
-    image1 : "http://campus.sf-tech.com.cn/wechat/asset/start-btn.png",
-    image2 : "http://campus.sf-tech.com.cn/wechat/asset/start.png",
-    image3 : "http://campus.sf-tech.com.cn/wechat/asset/last.jpg",
-    image4 : "http://campus.sf-tech.com.cn/wechat/asset/logo.png",
-    image5 : "http://campus.sf-tech.com.cn/wechat/asset/last/astronaut-body.png",
-    image6 : "http://campus.sf-tech.com.cn/wechat/asset/last/astronaut-head.png",
-    image7 : "http://campus.sf-tech.com.cn/wechat/asset/last/astronaut-light.png",
-    image8 : "http://campus.sf-tech.com.cn/wechat/asset/last/bg.jpg",
-    image9 : "http://campus.sf-tech.com.cn/wechat/asset/last/bottom-cloud.png",
-    image10 : "http://campus.sf-tech.com.cn/wechat/asset/last/bottom-element.png",
-    image11 : "http://campus.sf-tech.com.cn/wechat/asset/last/dec-image.png",
-    image12 : "http://campus.sf-tech.com.cn/wechat/asset/last/far-cloud.png",
-    image13 : "http://campus.sf-tech.com.cn/wechat/asset/last/footer-cloud.png",
-    image14 : "http://campus.sf-tech.com.cn/wechat/asset/last/middle-cloud.png",
-    image15 : "http://campus.sf-tech.com.cn/wechat/asset/last/speed-light.png",
-    image16 : "http://campus.sf-tech.com.cn/wechat/asset/last/title-light.png",
-    image17 : "http://campus.sf-tech.com.cn/wechat/asset/last/title.png",
-    image18 : "http://campus.sf-tech.com.cn/wechat/asset/last/top-element.png",
-    image19 : "http://campus.sf-tech.com.cn/wechat/asset/last/top-left-cloud.png",
-    image20 : "http://campus.sf-tech.com.cn/wechat/asset/last/ufo.png",
-    image21 : "http://campus.sf-tech.com.cn/wechat/asset/last/aircraft.png",
-    image22 : "http://campus.sf-tech.com.cn/wechat/asset/last/aircraft-light.png",
-    image23 : "http://campus.sf-tech.com.cn/wechat/asset/last/car.png",
-    image24 : "http://campus.sf-tech.com.cn/wechat/asset/last/car-light.png",
-    image25 : "http://campus.sf-tech.com.cn/wechat/asset/last/rocket-light.png",
-    image26 : "http://campus.sf-tech.com.cn/wechat/asset/last/sf-card-light.png",
-    image27 : "http://campus.sf-tech.com.cn/wechat/asset/last/btn-bg.png",
-    image28 : "http://campus.sf-tech.com.cn/wechat/asset/last/btn-icon.png"
+    image1 : "http://omu538iq8.bkt.clouddn.com/frontend/loading/img01.jpg",
+    image2 : "http://omu538iq8.bkt.clouddn.com/frontend/loading/img02.jpg",
+    image3 : "http://omu538iq8.bkt.clouddn.com/frontend/loading/img03.jpg",
+    image4 : "http://omu538iq8.bkt.clouddn.com/frontend/loading/img04.jpg",
+    image5 : "http://omu538iq8.bkt.clouddn.com/frontend/loading/img05.jpg",
+    image6 : "http://omu538iq8.bkt.clouddn.com/frontend/loading/img06.jpg",
+    image7 : "http://omu538iq8.bkt.clouddn.com/frontend/loading/img07.jpg",
+    image8 : "http://omu538iq8.bkt.clouddn.com/frontend/loading/img08.jpg",
+    image9 : "http://omu538iq8.bkt.clouddn.com/frontend/loading/img09.jpg",
+    image10 : "http://omu538iq8.bkt.clouddn.com/frontend/loading/img10.jpg",
+    image11 : "http://omu538iq8.bkt.clouddn.com/frontend/loading/img11.jpg",
+    image12 : "http://omu538iq8.bkt.clouddn.com/frontend/loading/img12.jpg",
+    image13 : "http://omu538iq8.bkt.clouddn.com/frontend/loading/img13.jpg",
+    image14 : "http://omu538iq8.bkt.clouddn.com/frontend/loading/img14.jpg",
+    image15 : "http://omu538iq8.bkt.clouddn.com/frontend/loading/img15.jpg",
+    image16 : "http://omu538iq8.bkt.clouddn.com/frontend/loading/img16.jpg",
+    image17 : "http://omu538iq8.bkt.clouddn.com/frontend/loading/img17.jpg",
+    image18 : "http://omu538iq8.bkt.clouddn.com/frontend/loading/img18.jpg",
+    image19 : "http://omu538iq8.bkt.clouddn.com/frontend/loading/img19.jpg",
+    image20 : "http://omu538iq8.bkt.clouddn.com/frontend/loading/img20.jpg",
+    image21 : "http://omu538iq8.bkt.clouddn.com/frontend/loading/img21.jpg",
   }
